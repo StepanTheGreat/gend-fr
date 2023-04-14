c90d92186bbdf8570ca189eef0734fc71bf8684d
Stepan <stepan@debian-BULLSEYE-live-builder-AMD64>
2023-04-14 19:21:33 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gend-fr',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
