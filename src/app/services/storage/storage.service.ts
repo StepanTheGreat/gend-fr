5d76f34de24c58d4dd5c3d44f7c18062114def2d
StepanTheGreat <grettanumber900@gmail.com>
2023-04-26 17:27:18 +0200

    Basic dictionary loading

--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -20,8 +20,7 @@ type AppData = {
 })
 export class StorageService {
 
-  dictVersion: string = "";
-  dict?: DictionaryType;
+  appData?: AppData;
 
   constructor(
     private storage: istorage.Storage,
@@ -36,38 +35,55 @@ export class StorageService {
   loadData() {
     this.get("appData").then((appData) => {
       if (appData) {
-        this.dictVersion = appData.dictVersion;
-        this.dict = appData.dict;      
+        this.appData = appData;
       } else {
         this.initSTD();
       }
-    });
-
-    const docRef = doc(this.afStore, "config/dictionary");
-    getDoc(docRef).then((docSnapshot) => {
-      if (docSnapshot.exists()) {
-        const data = docSnapshot.data();
-        const version = data["version"];
-
-        if (this.dictVersion != version) {
-          const dictRef = fstorage.ref(this.afStorage, "words.json");
-          // let dictBytes = fstorage.getBytes(dictRef);
-          //console.log(dictBytes);
-        }
-      }
+      this.checkDictUpdates(this.appData!);
     });
   }
 
   initSTD() {
     // Standard application initialisation
-    this.set("appData", {
+    this.appData = {
       dictVersion: "0.0.0",
       dict: {
         feminine: [],
         masculine: [],
         plural: []
       }
-    })
+    };
+    this.set("appData", this.appData);
+  }
+
+  checkDictUpdates(appData: AppData) {
+    const docRef = doc(this.afStore, "config/dictionary");
+    getDoc(docRef).then((docSnapshot) => {
+      if (docSnapshot.exists()) {
+        const data = docSnapshot.data();
+        const version = data["version"];
+
+        if (this.appData?.dictVersion != version) {
+          const dictRef = fstorage.ref(this.afStorage, "words.json");
+          fstorage.getBytes(dictRef).then((bytes) => {
+            const bstring = new TextDecoder().decode(bytes);
+            let parsedDict = JSON.parse(bstring);
+
+            appData.dict = parsedDict;
+            appData.dictVersion = version;
+
+            this.appData = appData;
+
+            this.saveData();
+            console.log(parsedDict);            
+          });
+        }
+      }
+    });
+  }
+
+  saveData() {
+    this.set("appData", this.appData);
   }
 
   async get(key: string): Promise<any> {
